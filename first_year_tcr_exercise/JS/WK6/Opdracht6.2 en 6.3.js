bbf731e30d5307e74cc65c9b3f84f748b88ee5f7
22731K <9012883@student.zadkine.nl>
2022-10-20 13:57:26 +0200

    JS WK6

---
+++ first_year_tcr_exercise/JS/WK6/Opdracht6.2 en 6.3.js
@@ -0,0 +1,7 @@
+function myFunction(){
+    document.getElementById("title").style.color = "red";
+}
+function myFunction1(){
+    var h1=document.getElementById("title");
+    h1.innerHTML="Hallo world"
+}
