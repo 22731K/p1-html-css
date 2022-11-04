aeef0454fadf9b6fe07343a1f25b5e30b6347b68
22731K <9012883@student.zadkine.nl>
2022-11-04 12:05:15 +0100

    opzet Opdracht8.1

---
+++ first_year_tcr_exercise/JS/WK8/8.1.js
@@ -0,0 +1,24 @@
+function Plus(){
+    var getal1=parseInt(document.getElementById('getal1').value);
+    var getal2=parseInt(document.getElementById('getal2').value);
+    var antwoord=getal1+getal2;
+   alert(antwoord)
+}
+function Min(){
+    var getal1=document.getElementById('getal1').value;
+    var getal2=document.getElementById('getal2').value;
+    var antwoord=getal1-getal2
+   alert(antwoord)
+}
+function Keer(){
+    var getal1=document.getElementById('getal1').value;
+    var getal2=document.getElementById('getal2').value;
+    var antwoord=getal1*getal2
+   alert(antwoord)
+}
+function Delen(){
+    var getal1=document.getElementById('getal1').value;
+    var getal2=document.getElementById('getal2').value;
+    var antwoord=getal1/getal2
+   alert(antwoord)
+}
