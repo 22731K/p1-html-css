7d2fc1682813b25e12703a82955be96250e30be2
22731K <9012883@student.zadkine.nl>
2022-11-04 12:22:41 +0100

    Opdracht 8.1

--- first_year_tcr_exercise/JS/WK8/8.1.js
+++ first_year_tcr_exercise/JS/WK8/8.1.js
@@ -2,7 +2,7 @@ function Plus(){
     var getal1=parseInt(document.getElementById('getal1').value);
     var getal2=parseInt(document.getElementById('getal2').value);
     var antwoord=getal1+getal2;
-   alert(antwoord)
+   document.getElementById("antwoord").innerHTML=antwoord;
 }
 function Min(){
     var getal1=document.getElementById('getal1').value;
