29abb956265452dcd97406d89e2f1a4edced1c8d
22731K <9012883@student.zadkine.nl>
2022-11-07 11:04:31 +0100

    opdracht8.2

---
+++ first_year_tcr_exercise/JS/WK8/8.2.js
@@ -0,0 +1,36 @@
+function Plus(){
+    var getal1=parseInt(document.getElementById('getal1').value);
+    var getal2=parseInt(document.getElementById('getal2').value);
+    var antwoord=getal1+getal2;
+    document.getElementById("antwoord").innerHTML=antwoord;
+ if  (antwoord < 1) {
+    document.getElementById("antwoord").innerHTML = "Het getal is te laag"
+   }
+}
+function Min(){
+    var getal1=document.getElementById('getal1').value;
+    var getal2=document.getElementById('getal2').value;
+    var antwoord=getal1-getal2
+   document.getElementById("antwoord").innerHTML=antwoord;
+   if  (antwoord < 1) {
+    document.getElementById("antwoord").innerHTML = "Het getal is te laag"
+   }
+}
+function Keer(){
+    var getal1=document.getElementById('getal1').value;
+    var getal2=document.getElementById('getal2').value;
+    var antwoord=getal1*getal2
+    document.getElementById("antwoord").innerHTML=antwoord;
+    if  (antwoord < 1) {
+        document.getElementById("antwoord").innerHTML = "Het getal is te laag"
+       }
+}
+function Delen(){
+    var getal1=document.getElementById('getal1').value;
+    var getal2=document.getElementById('getal2').value;
+    var antwoord=getal1/getal2
+    document.getElementById("antwoord").innerHTML=antwoord;
+    if  (antwoord < 1) {
+        document.getElementById("antwoord").innerHTML = "Het getal is te laag"
+       }
+}
