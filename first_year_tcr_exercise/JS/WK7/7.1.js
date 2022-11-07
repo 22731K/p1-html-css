cba422aa80a5ca6d007ca8128c2a448ea510ae2f
22731K <9012883@student.zadkine.nl>
2022-11-07 12:13:04 +0100

    Opdracht7.1

---
+++ first_year_tcr_exercise/JS/WK7/7.1.js
@@ -0,0 +1,23 @@
+function opdracht71() {
+
+    var budget = 150;
+
+    var product = 100;
+
+
+
+    if (budget > product) {
+
+        document.getElementById("budget").innerHTML = "budget is goed";
+
+      }
+
+      else {
+
+        document.getElementById("budget").innerHTML = "budget is laag";
+
+      }
+
+
+
+}
